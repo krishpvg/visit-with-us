c10c46c9e0c059e80968939ccf572ab0a7f2519a
krishpvg <krish.pvg@gmail.com>
2025-12-23 20:45:50 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="mlops_project-2/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id="krishpvg/visit-with-us",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
